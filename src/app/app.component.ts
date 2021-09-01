d6602ea57beb7df1a7ccc31e2c2becca3504a3dc
alexander97olsson <alexander93olsson@hotmail.com>
2021-09-01 11:12:01 +0100

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alex-editor-program';
+  public Editor = ClassicEditor;
+  consoleMessage = '';
+
+  public onChange( { editor }: ChangeEvent ) {
+    const data = editor.getData();
+    this.consoleMessage = data;
+  }
+
+  onClickMe() {
+    console.log( this.consoleMessage);
+  }
 }
