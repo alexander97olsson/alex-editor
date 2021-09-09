60b9fcf0d241e127d5de144b91441f9ab8d7db63
alexander97olsson <alexander93olsson@hotmail.com>
2021-09-09 08:39:08 +0100

    you can now update existing docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import {Injectable, Renderer2, RendererFactory2} from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -11,14 +12,19 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'alex-editor-program';
   public Editor = ClassicEditor;
-  public objectDocs = [];
+  public objectDocs = {} as any;
+  public objectDocs123 = {} as any;
+  public nameArray: string[] = [];
+  public selected = "";
+  public consoleMessage = '';
   public model = {
     editorDataTitle: '<p>Hello, world!</p>',
     editorDataText: '<p>Hello, world!</p>'
   };
-  constructor(private router: Router) { }
-
-  consoleMessage = '';
+  
+  constructor (private renderer: Renderer2) {
+    this.onClickGetAll();
+  }
 
   mainText = new FormControl('');
   titleText = new FormControl('');
@@ -30,13 +36,30 @@ export class AppComponent {
   updateTitleText() {
     this.titleText.setValue(this.model.editorDataTitle);
   }
-
+  public onChangeTitle(data) {
+    this.model.editorDataTitle = data.target.value;
+    console.log(this.model.editorDataTitle);
+  }
   public onChange( { editor }: ChangeEvent ) {
     const data = editor.getData();
     this.consoleMessage = data;
     this.model.editorDataText = data;
+    console.log(this.titleText.value);
   }
 
+  async onClickLoad() {
+    const middleHand = await this.getAll();
+    this.model.editorDataText = "";
+    for (let i = 0; i < middleHand.data.msg.length; i++) {
+      if(this.selected == middleHand.data.msg[i]._id) {
+        this.model.editorDataText = middleHand.data.msg[i].maintext;
+        this.model.editorDataTitle = middleHand.data.msg[i].title
+      }
+    }
+    this.updateMainText();
+    this.updateTitleText();
+  }
+  
   public async getAll() {
     try {
       const response = await fetch(`https://ramverk-editor-alos17.azurewebsites.net/data`, {
@@ -52,24 +75,20 @@ export class AppComponent {
     }
   }
   async onClickGetAll() {
-    const objectDocs = await this.getAll();
-    console.log(objectDocs.data.msg[0].title);
-    console.log(objectDocs.data.msg[0]._id);
-    for (let i = 0; i < objectDocs.data.msg.length; i++) {
-      console.log(objectDocs.data.msg[i].maintext);
-      this.model.editorDataText += objectDocs.data.msg[i].maintext;
-    }
-    this.model.editorDataTitle = objectDocs.data.msg[0].title;
+    this.objectDocs = await this.getAll();
+    this.objectDocs123 = this.objectDocs.data;
     this.updateMainText();
     this.updateTitleText();
-    this.router.navigate(['/']);
   }
 
   onClickSubmit(data) {
+    this.updateMainText();
+    this.updateTitleText();
     console.log("title is : " + data.titleText);
+    console.log("object is : " + data);
     var delivery = {
-      title: data.titleText,
-      maintext: this.consoleMessage
+      title: this.model.editorDataTitle,
+      maintext: this.model.editorDataText
     };
     fetch(`https://ramverk-editor-alos17.azurewebsites.net/data`, {
       body: JSON.stringify(delivery),
@@ -83,4 +102,29 @@ export class AppComponent {
       console.log(data)
     })
  }
+
+ async updateSpecific() {
+  const middleHand = await this.getAll();
+
+   for (let i = 0; i < middleHand.data.msg.length; i++) {
+    if(this.selected == middleHand.data.msg[i]._id) {
+      var delivery = {
+        __id: this.selected,
+        title: this.model.editorDataTitle,
+        maintext: this.model.editorDataText
+      };
+      fetch("https://ramverk-editor-alos17.azurewebsites.net/data", {
+        body: JSON.stringify(delivery),
+        headers: {
+          'content-type': 'application/json'
+        },
+        method: 'PUT'
+        }).then(function (response) {
+          return response.json();
+        }).then(function(data) {
+          console.log(data)
+        })
+     }
+    }
+   }
 }
